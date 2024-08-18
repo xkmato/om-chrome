414e9896c2724644f1757e598006676612cadee7
xkmato <kenneth@uvotam.com>
2024-08-18 18:58:57 +0300

    more stuff

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.includes('twitter.com')) {
+    if (changeInfo.status === 'complete' && tab.url.includes('x.com')) {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
         files: ['content.js']
--- popup.js
+++ popup.js
@@ -1,35 +1,63 @@
-document.getElementById('saveSettings').addEventListener('click', () => {
-    let url = document.getElementById('urlInput').value;
-    let scrollAmount = document.getElementById('scrollAmount').value;
+document.getElementById("saveSettings").addEventListener("click", () => {
+  let url = document.getElementById("urlInput").value;
+  let numberOfTweets = document.getElementById("numberOfTweets").value;
+  let llmApiKey = document.getElementById("llmApiKey").value;
 
-    if (url && scrollAmount) {
-        chrome.storage.local.set({
-            tweetPostUrl: url,
-            scrollAmount: parseInt(scrollAmount, 10)
-        }, () => {
-            document.getElementById('status').textContent = 'Settings saved successfully!';
-        });
-    } else {
-        document.getElementById('status').textContent = 'Please enter valid settings.';
-    }
+  if (url && numberOfTweets && llmApiKey) {
+    chrome.storage.local.set(
+      {
+        tweetPostUrl: url,
+        numberOfTweets: parseInt(numberOfTweets, 200),
+        llmApiKey: llmApiKey,
+      },
+      () => {
+        document.getElementById("status").textContent =
+          "Settings saved successfully!";
+      }
+    );
+  } else {
+    document.getElementById("status").textContent =
+      "Please enter valid settings.";
+  }
 });
 
-document.getElementById('start').addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "startScrolling" });
-    });
+document.getElementById("start").addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "startScrolling" });
+  });
 });
 
+document.getElementById("stop").addEventListener("click", function () {
+  const waitingForRecomendations = "Waiting for Recomendations...";
+  // Clear the ollama response in storage
+  chrome.storage.local.set({
+    ollamaResponse: waitingForRecomendations,
+  });
+  document.getElementById("serverResponse").innerHTML =
+    waitingForRecomendations;
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "stopScrolling" });
+  });
+});
 
 // Load the saved settings and Ollama response when the popup opens
-chrome.storage.local.get(['tweetPostUrl', 'scrollAmount', 'ollamaResponse'], (result) => {
-    if (result.tweetPostUrl) {
-        document.getElementById('urlInput').value = result.tweetPostUrl;
+setInterval(() => {
+  chrome.storage.local.get(
+    ["tweetPostUrl", "scrollAmount", "ollamaResponse"],
+    (result) => {
+      if (result.tweetPostUrl) {
+        document.getElementById("urlInput").value = result.tweetPostUrl;
+      }
+      if (result.scrollAmount) {
+        document.getElementById("scrollAmount").value = result.scrollAmount;
+      }
+      if (result.ollamaResponse) {
+        const htmlContent = marked.parse(result.ollamaResponse);
+        if (result.ollamaResponse) {
+          const htmlContent = marked.parse(result.ollamaResponse);
+          document.getElementById("serverResponse").innerHTML = htmlContent;
+        }
+      }
     }
-    if (result.scrollAmount) {
-        document.getElementById('scrollAmount').value = result.scrollAmount;
-    }
-    if (result.ollamaResponse) {
-        document.getElementById('serverResponse').value = result.ollamaResponse;
-    }
-});
+  );
+}, 5000); // Check for changes every 1 second
